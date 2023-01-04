97493fc973c8900e2ea9207c0b54adf1f7595a5a
Saniago <velasquezsantiago0412@gmail.com>
2023-01-03 21:36:02 -0500

    entregable 2 terminado

--- poke_api.py
+++ poke_api.py
@@ -3,25 +3,24 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-def controller_poke_api(headers):
-    response = response.json()
+def controller_poke(headers):
 
-try:
-    endpoint_poke_api = headers['poke_api']
-    exists_ability_name = headers['ability_name']
+    try:
+        endpoint_poke_api = headers['poke_api']
+        exists_ability_name = headers['ability_name']
 
-    response = requests.get(endpoint_poke_api)
-    response = response.json()
+        response = requests.get(endpoint_poke_api)
+        response = response.json()
 
-    abilities = response['abilities'][0]
-    ability_name = abilities['ability']['name']
+        abilities = response['abilities'][0]
+        ability_name = abilities['ability']['name']
 
-except Exception as e:
-    return {'error':e.args[0]}, 400
-else:
-    if exists_ability_name in ability_name:
-        return {'This pokemon does have this ability'}, 200
-    return {'This pokemon does not have this ability'}, 200
+    except Exception as e:
+        return {'error':e.args[0]}, 400
+    else:
+        if exists_ability_name in ability_name:
+            return {'This pokemon does have this ability':True}, 200
+        return {'This pokemon does not have this ability':False}, 200
 
 @app.route('/api/v2/poke')
 def poke():
